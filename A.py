91b021017808fb6c1ad3f11c516e0061066d1964
CEJ <artarthur29@naver.com>
2023-02-02 14:33:15 +0900

    First Test

---
+++ A.py
@@ -0,0 +1,15 @@
+import matplotlib
+
+#print(matplotlib.__version__) #3.6.3
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+xp = np.array([0,5])
+yp = np.array([0,300])
+#print(xp)
+#print(yp)
+
+#plt.plot(xp, yp) #line
+plt.plot(xp, yp, 'o') #점
+plt.show()
